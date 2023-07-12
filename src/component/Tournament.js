7515fe1bb8ddde025a600332a5fb9546644fb393
febin <febnmathew98@gmail.com>
2023-07-12 12:28:44 +0530

    data fetching and mapping completed..color change in feedback component

--- src/component/Tournament.js
+++ src/component/Tournament.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import { Post } from './service/Api';
 
 function Tournament() {
-  return (
+
+
+   const [tournaments, setTournaments] = useState([]);
+
+   useEffect(()=> {
+
+      Post('getalltournaments', {tablename:"tournaments"}).then((data) => {
+         setTournaments(data);
+         console.log(data);
+      })
+
+   }, []);
+
+
+   return (
     <>
     <Header/>
     <div className="team-holder theme-padding">
@@ -11,230 +26,31 @@ function Tournament() {
             <div className="main-heading sytle-2">
               <h2>Tournaments</h2>
               <div style={subContainer}>
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
+
+
+               {tournaments?.map((tournament,index)=> {
+                  return(
+                <div key={index} style={card}>
+                   <h4 style={title}>{tournament.name}</h4>
                    <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
+                   <h5>From: {tournament.datefrom}</h5>
+                   <h5>To: {tournament.dateto}</h5>
                    </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
+                   <h5>Time: {tournament.time} </h5>
+                   <h5>Host: {tournament.username}</h5>
+                   <h6>Location: {tournament.address}</h6>
                    <button style={bookButton}>
                     Book Ticket
                    </button>
                    
                 </div>
+
+                  )
+               })}
+                
                
                 
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
                 
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
-               
-                
-                <div style={card}>
-                   <h4 style={title}>All Kerala U20</h4>
-                   <div style={dateContainer}>
-                   <h5>From: 2023-01-08</h5>
-                   <h5>To: 2023-01-09</h5>
-                   </div>
-                   <h5>Time: 	01:00:00</h5>
-                   <h5>Host: 	Turf name</h5>
-                   <button style={bookButton}>
-                    Book Ticket
-                   </button>
-                   
-                </div>
                
 
               </div>
@@ -265,7 +81,7 @@ const subContainer = {
 };
 
 const card = {
-    height: '180px',
+    minHeight: '180px',
     width: '350px',
     backgroundColor:"white",
     margin:"20px",
@@ -287,7 +103,8 @@ const bookButton = {
     border: "none",
     padding: "6px 60px",
     borderRadius: "25px",
-    fontWeight: '600'
+    fontWeight: '600',
+    marginBottom : '10px'
 };
 const title = {
     fontWeight:"600"
